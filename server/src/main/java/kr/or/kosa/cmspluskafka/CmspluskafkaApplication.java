505eadf64932a076608a77c0438869f96426078a
rlatkd <rlatkdgns042@naver.com>
2024-08-13 16:55:56 +0900

    0813

---
+++ server/src/main/java/kr/or/kosa/cmspluskafka/CmspluskafkaApplication.java
@@ -0,0 +1,13 @@
+package kr.or.kosa.cmspluskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmspluskafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmspluskafkaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/kr/or/kosa/cmspluskafka/CmspluskafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.kosa.cmspluskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmspluskafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
